fb399feac8df09c58910094c2e04c20cd2504c7e
hulianghao <1647810838@qq.com>
2026-03-22 21:04:35 +0800

    Fix 500 error: add DOMMatrix polyfill for pdf-parse

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,10 @@
+// Polyfill for pdf-parse (it expects browser APIs like DOMMatrix in some environments)
+if (typeof (global as any).DOMMatrix === 'undefined') {
+  (global as any).DOMMatrix = class DOMMatrix {
+    constructor() {}
+  };
+}
+
 import app from '../server/index.js';
 
 export default app;
